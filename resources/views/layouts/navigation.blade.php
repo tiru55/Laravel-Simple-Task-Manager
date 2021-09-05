0bd6f0e43d7a9cc143165b83c008a12407f792e6
tiru55 <tirumalgowdsonti@gmail.com>
2021-09-05 15:59:03 +0530

    commit on 'main'

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,7 +27,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->first_name }}</div>
+                            <div><img src="https://ui-avatars.com/api/?name={{Auth::user()->first_name}}+{{Auth::user()->last_name}}&amp;color=7F9CF5&amp;background=EBF4FF" class="h-10 w-10 rounded-full object-cover" alt="profilepic" /></div>
 
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
